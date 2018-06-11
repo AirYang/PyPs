984229aef313ef465076155fbcdf27deb4c5b39c
AirYang <yangguang.china@aliyun.com>
2018-06-11 11:23:44 +0800

    add tcp maimon

--- PortScanner.py
+++ PortScanner.py
@@ -10,44 +10,102 @@ from util.TcpFin import TcpFin
 from util.TcpNull import TcpNull
 from util.TcpXmas import TcpXmas
 from util.TcpAck import TcpAck
+from util.TcpMaimon import TcpMaimon
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 4:
-        print "./PortScanner.py DestIp MinPort MaxPort"
+    if len(sys.argv) != 5:
+        print "./PortScanner.py TypeNumber DestIp MinPort MaxPort"
+        print "type number include: "
+        print "              1 ping ip"
+        print "              2 tcp connect scan ip from min port to max port"
+        print "              3 tcp syn scan ip from min port to max port"
+        print "              4 udp scan ip from min port to max port"
+        print "              5 tcp fin scan ip from min port to max port"
+        print "              6 tcp null scan ip from min port to max port"
+        print "              7 tcp xmas scan ip from min port to max port"
+        print "              8 tcp ack scan ip from min port to max port"
+        print "              9 tcp maimon scan ip from min port to max port"
         exit()
 
-    minPort = int(sys.argv[2])
-    maxPort = int(sys.argv[3])
+    typeNumber = int(sys.argv[1])
+    destIp = sys.argv[2]
+    minPort = int(sys.argv[3])
+    maxPort = int(sys.argv[4])
     # print minPort, maxPort
 
-    # pinger = Ping()
-    # pinger.verbose_ping(sys.argv[1])
+    if typeNumber == 1:
+        # test www.baidu.com 220.181.112.244 good
+        # test noj.cn 192.168.0.242 bad
+        print "--- Icmp Ping ---"
+        pinger = Ping()
+        pinger.verbose_ping(destIp)
 
-    # tcpConnect = TcpConnect(
-    #     desc="--- Tcp Connect %s ---", works=4, timeout=0.5)
-    # tcpConnect.scanSingleHost(
-    #     sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 2:
+        # test www.baidu.com 220.181.112.244 (80 443 open begin)
+        # test noj.cn 192.168.0.242 (80 3389 open)
+        tcpConnect = TcpConnect(
+            desc="--- Tcp Connect %s ---", works=4, timeout=0.5)
+        tcpConnect.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    # # test github.com and with no ping
-    # tcpSyn = TcpSyn(desc="--- Tcp Syn %s ---", works=4, timeout=0.5)
-    # tcpSyn.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 3:
+        # test www.baidu.com 220.181.112.244 (80 443 open)
+        # test noj.cn 192.168.0.242 (80 3389 open)
+        # test www.taobao.com 58.205.221.214 (80 443 open)
+        tcpSyn = TcpSyn(desc="--- Tcp Syn %s ---", works=4, timeout=0.5)
+        tcpSyn.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    # test 117.34.105.104
-    # udpScan = UdpScan(desc="--- Udp Scan %s ---", works=1, timeout=1.0)
-    # udpScan.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 4:
+        # test Xi'an 117.34.105.104 (445 448 450 not recv)
+        # test noj.cn 192.168.0.242 (all not recv)
+        # test www.baidu.com 220.181.112.244 (all not recv)
+        udpScan = UdpScan(desc="--- Udp Scan %s ---", works=1, timeout=1.0)
+        udpScan.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    # test 192.168.0.242
-    # test 221.231.138.56
-    # test 202.117.94.248 can test
-    # tcpFin = TcpFin(desc="--- Tcp Fin %s ---", works=4, timeout=0.5)
-    # tcpFin.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 5:
+        # test noj.cn 192.168.0.242 (80 3389 recv ack rst)
+        # test Xi'an 117.34.105.104 (not recv all)
+        # test www.nwpu.edu.cn 202.117.94.248 (recv ack rst all)
+        tcpFin = TcpFin(desc="--- Tcp Fin %s ---", works=4, timeout=0.5)
+        tcpFin.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    # tcpNull = TcpNull(desc="--- Tcp Null %s ---", works=4, timeout=0.5)
-    # tcpNull.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 6:
+        # test noj.cn 192.168.0.242 (80 3389 recv ack rst)
+        # test www.nwpu.edu.cn 202.117.94.248 (recv ack rst all)
+        # test github.com 13.250.177.223 (not recv all)
+        # test www.taobao.com 58.205.221.214 (not recv all)
+        tcpNull = TcpNull(desc="--- Tcp Null %s ---", works=4, timeout=0.5)
+        tcpNull.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    # tcpXmas = TcpXmas(desc="--- Tcp Xmas %s ---", works=4, timeout=0.5)
-    # tcpXmas.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 7:
+        # test noj.cn 192.168.0.242 (80 3389 recv ack rst)
+        # test www.nwpu.edu.cn 202.117.94.248 (recv ack rst all)
+        # test BeiJing 123.206.91.84 (not recv all)
+        tcpXmas = TcpXmas(desc="--- Tcp Xmas %s ---", works=4, timeout=0.5)
+        tcpXmas.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
 
-    tcpAck = TcpAck(desc="--- Tcp Ack Win %s ---", works=4, timeout=0.5)
-    tcpAck.scanSingleHost(sys.argv[1], [x for x in range(minPort, maxPort+1)])
+    elif typeNumber == 8:
+        # test noj.cn 192.168.0.242 (80 3389 recv rst with win 0)
+        # test www.nwpu.edu.cn 202.117.94.248 (recv all)
+        # test  www.runoob.com 125.76.247.179 (not recv all)
+        tcpAck = TcpAck(desc="--- Tcp Ack Win %s ---", works=4, timeout=0.5)
+        tcpAck.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
+
+    elif typeNumber == 9:
+        # test noj.cn 192.168.0.242 (80 3389 recv rst)
+        # test www.nwpu.edu.cn 202.117.94.248 (recv all)
+        tcpMaimon = TcpMaimon(
+            desc="--- Tcp Maimon %s ---", works=4, timeout=0.5)
+        tcpMaimon.scanSingleHost(
+            destIp, [x for x in range(minPort, maxPort+1)])
+
+    else:
+        print "not include this type number\n"
+        exit()
---
+++ util/TcpMaimon.py
@@ -0,0 +1,125 @@
+import socket
+import struct
+import random
+import threading
+import Scanner
+import NetworkHelp
+
+
+class TcpMaimon(Scanner.Scanner):
+
+    def _scanPort(self, hostIp, port):
+
+        try:
+            s = NetworkHelp.createTcpRawSocket()
+            s.settimeout(self.timeout)
+
+            # print "test0"
+
+            # packet = ''
+            sourceIp = NetworkHelp.getHostIp()
+            destIp = hostIp
+
+            # print "test1"
+
+            # ip header
+            # ipVersion = 4
+            # ipHLength = 5
+            # ipService = 0x00
+            # ipTotalLength = 40
+            # ipId = int(random.uniform(20000, 30000))
+            # ipFlags = 0  # int('010', 2)
+            # ipFragOff = 0
+            # ipTtl = int(random.uniform(50, 128))
+            # ipProtocol = socket.IPPROTO_TCP
+            # ipCheckSum = 0
+            # ipSourceIp = socket.inet_aton(sourceIp)
+            # ipDestIp = socket.inet_aton(destIp)
+            # ipPayload = ''
+
+            # print "test2"
+
+            ipHeader = NetworkHelp.getIpHeader(
+                sourceIp=sourceIp, destIp=destIp)
+
+            # struct.pack(
+            #     '!BBHHHBBH4s4s', (ipVersion << 4) + ipHLength, ipService, ipTotalLength, ipId,  (ipFlags << 13) + ipFragOff, ipTtl, ipProtocol, ipCheckSum, ipSourceIp, ipDestIp)
+
+            # print "test3"
+
+            # tcp header
+            # tcpSourcePort = int(random.uniform(10000, 60000))
+            # tcpDestPort = port
+            # tcpSeqNum = 1
+            # tcpAckNum = 0
+            # tcpDataOff = 5
+            # tcpUrg = 0
+            # tcpAck = 0
+            # tcpPsh = 0
+            # tcpRst = 0
+            # tcpSyn = 0
+            # tcpFin = 1
+            # tcpWinSize = 1024
+            # tcpCheckSum = 0
+            # tcpUrgPtr = 0
+
+            # tcpDataOffAndRes = (tcpDataOff << 4) + 0
+            # tcpFlags = tcpFin+(tcpUrg << 5) + (tcpAck << 4) + \
+            #     (tcpPsh << 3) + (tcpRst << 2) + (tcpSyn << 1)
+
+            # tcpHeader = struct.pack(
+            #     "!HHLLBBHHH", tcpSourcePort, tcpDestPort, tcpSeqNum, tcpAckNum, tcpDataOffAndRes, tcpFlags, tcpWinSize, tcpCheckSum, tcpUrgPtr)
+
+            # tcpPreHeader = struct.pack(
+            #     "!4s4sBBH", ipSourceIp, ipDestIp, 0, socket.IPPROTO_TCP, len(tcpHeader))
+
+            # # print "test6"
+            # tcpCheckSum = NetworkHelp.checksum(tcpPreHeader + tcpHeader)
+
+            # print "test5"
+            tcpHeader = NetworkHelp.getTcpHeader(urg=0,
+                                                 ack=1, psh=0, rst=0, syn=0, fin=1, sourceIp=sourceIp, destIp=destIp, destPort=port,
+                                                 seqnum=1,
+                                                 acknum=0)
+
+            # struct.pack(
+            #     "!HHLLBBHHH", tcpSourcePort, tcpDestPort, tcpSeqNum, tcpAckNum, tcpDataOffAndRes, tcpFlags, tcpWinSize, tcpCheckSum, tcpUrgPtr)
+
+            # print "test4"
+            s.sendto(ipHeader + tcpHeader, (destIp, 0))
+
+            # print "sendto"
+            # pass
+
+            while True:
+                recvData, recvAddr = s.recvfrom(65535)
+                # print recvAddr, destIp
+
+                if recvAddr[0] == destIp:
+                    recvSourcePort = struct.unpack(
+                        '!H', recvData[20:22])[0]
+
+                    # print destIp
+
+                    recvFlags = struct.unpack('!B', recvData[33:34])[0]
+
+                    # print recvFlags
+                    # print recvFlags
+                    # recvRst = (recvFlags & int('00000100', 2)) != 0
+                    # port and rst ack
+                    if (recvSourcePort == port) and (recvFlags == 4):
+                        self.printMutex.acquire()
+                        print "[%d] recv rst" % port
+                        self.printMutex.release()
+                        # print "ip port"
+                        break
+                    # print recvSourcePort
+                    # break
+
+        except:
+            self.printMutex.acquire()
+            print "[%d] not recv rst" % port
+            self.printMutex.release()
+        finally:
+            # pass
+            s.close()
